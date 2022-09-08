933dd7f18f8e25312de871d851b61784a1dfd00b
duynghia-123 <duynghia22302@gmail.com>
2022-09-09 06:30:25 +0700

    Updates

---
+++ precache-manifest.6ebd04d72453cf0428d44ae2a36fc58e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5793188005db1c82d10ab30521eec0cd",
+    "url": "/react-corona-tracker-app/index.html"
+  },
+  {
+    "revision": "b9ba7dad4b1f3611cbbf",
+    "url": "/react-corona-tracker-app/static/css/main.425726a6.chunk.css"
+  },
+  {
+    "revision": "4fb8e2836a3506fa1aa2",
+    "url": "/react-corona-tracker-app/static/js/2.10c845ea.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-corona-tracker-app/static/js/2.10c845ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ba7dad4b1f3611cbbf",
+    "url": "/react-corona-tracker-app/static/js/main.48140c6c.chunk.js"
+  },
+  {
+    "revision": "c1037a356b26d25afd2e",
+    "url": "/react-corona-tracker-app/static/js/runtime-main.11b48820.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-corona-tracker-app/static/media/image.d7265326.png"
+  }
+]);
